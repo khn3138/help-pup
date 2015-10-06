a4fa5551bd748db8ce2b720d91ba56169d46bb14
kwon <kwon@kwon-PC>
2015-10-06 17:42:02 +0900

    first

---
+++ HelpPup/src/kr/ac/pup/help/HelpPupServlet.java
@@ -0,0 +1,13 @@
+package kr.ac.pup.help;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelpPupServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
